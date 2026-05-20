9d1b732811258faca5b7620bfc4f6cb72997c35f
FernandaManriquez <fe.manriquezm@duocuc.cl>
2026-05-20 14:59:43 -0400

    update backend ip

--- src/componentes/CrudAdmin/TableDespachos.jsx
+++ src/componentes/CrudAdmin/TableDespachos.jsx
@@ -8,7 +8,7 @@ export const TableDespachos = () => {
 
   const despacho = async () => {
     await axios
-      .get("http://100.55.6.70:8081/api/v1/despachos", {
+      .get("http://3.238.144.162:8081/api/v1/despachos", {
         headers:{
               'Content-Type': 'application/json',
               'Accept': 'application/json'
